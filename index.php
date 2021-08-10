345c7c57cd3cb950f987a4ea4547e8ee12b36b3c
Jose Diaz <josemanueldiazj@gmail.com>
2021-08-10 13:25:02 -0400

    2do. Diseño de Pagina de Registro

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+</head>
+<body>
+    <div class="wrapper">
+        <section class="form signup">
+            <header>Realtime Chat App</header>
+            <form action="#">
+                <div class="error-txt">This is a error message!</div>
+                <div class="name-details">
+                   <div class="field">
+                       <label for="">Primer Nombre</label>
+                       <input type="text" placeholder="Primer Nombre">
+                   </div> 
+                   <div class="field">
+                       <label for="">Apellido</label>
+                       <input type="text" placeholder="Apellido">
+                   </div> 
+                   <div class="field">
+                       <label for="">Email</label>
+                       <input type="text" placeholder="Aexample@gmail.com">
+                   </div> 
+                   <div class="field">
+                       <label for="">Password</label>
+                       <input type="text" placeholder="Nuevo Password">
+                   </div> 
+                   <div class="field">
+                       <label for="">Selecciona una Imagen </label>
+                       <input type="file">
+                   </div> 
+                   <div class="field">
+                       <input type="submit" value="Continuar al Chat">
+                   </div> 
+                </div>
+            </form>
+            <div class="link">Ya estas Registrado?<a href="#">Login</a></div>
+        </section>
+    </div>
+    
+</body>
+</html>
